e01aa0dac51bcd7b68523bcd7c7aad2ed6e351db
VINZ-tech26 <mathapativinay9@gmail.com>
2026-03-10 22:19:38 +0530

    Update ai.py

--- TASK1_TicTacToe_AI/ai.py
+++ TASK1_TicTacToe_AI/ai.py
@@ -1,1 +1,52 @@
+from game_logic import check_winner, is_draw
 
+def minimax(board, depth, is_maximizing):
+    
+    if check_winner("O"):
+        return 1
+    if check_winner("X"):
+        return -1
+    if is_draw():
+        return 0
+
+    if is_maximizing:
+        best_score = -1000
+
+        for i in range(9):
+            if board[i] == " ":
+                board[i] = "O"
+                score = minimax(board, depth + 1, False)
+                board[i] = " "
+                best_score = max(score, best_score)
+
+        return best_score
+
+    else:
+        best_score = 1000
+
+        for i in range(9):
+            if board[i] == " ":
+                board[i] = "X"
+                score = minimax(board, depth + 1, True)
+                board[i] = " "
+                best_score = min(score, best_score)
+
+        return best_score
+
+
+def ai_move(board):
+
+    best_score = -1000
+    move = None
+
+    for i in range(9):
+        if board[i] == " ":
+            board[i] = "O"
+            score = minimax(board, 0, False)
+            board[i] = " "
+
+            if score > best_score:
+                best_score = score
+                move = i
+
+    return move
